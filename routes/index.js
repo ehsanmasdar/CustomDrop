7413dabe6b34e18fcb3f8fe7a077883351cebdaa
Ehsan Asdar <ehsanmasdar@gmail.com>
2015-06-01 16:03:30 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var busboy = require('connect-busboy');
-
+var inspect = require('util').inspect;
 var router = express.Router();
 
 var Dropbox = require("dropbox");
@@ -13,7 +13,7 @@ var client = new Dropbox.Client({
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index.html');
 });
 router.get('/testfile', function(req, res, next) {
   client.writeFile("test.txt", "sometext", function (error, stat) {
@@ -22,21 +22,33 @@ router.get('/testfile', function(req, res, next) {
           return;
         }
     });
-    res.render('index', { title: 'Express' });
+    res.render('index.html');
 });
 router.post('/upload', function(req, res) {
-    var fstream;
+    var fstream,lastname,period,firstname,secret,assignment,extension;
     req.pipe(req.busboy);
-    req.busboy.on('file', function (fieldname, file, filename) {
-        console.log("Uploading: " + filename);
-        file.on('data', function(data) {
-            client.writeFile(filename, data, function (error, stat) {
-                if (error) {
-                  console.log(error);
-                  return;
-                 }
-            });
-        });
+    req.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+      	console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+      	if (fieldname == "lastname"){lastname = val;}
+        if (fieldname == "firstname"){firstname = val;}
+	if (fieldname == "period"){period = val;}
+	if (fieldname == "secret"){secret = val;}
+	if (fieldname == "assignment"){assignment = val;}
+	if (fieldname == "extension"){
+		req.busboy.on('file', function (fieldname, file, filename) {
+       		 console.log("Uploading: " + filename);
+       		 file.on('data', function(data) {
+		 var finalfilename = period + "/" + lastname + firstname + secret + "_" + assignment + "." + val;
+           	 client.writeFile(finalfilename, data, function (error, stat) {
+               		 if (error) {
+                	  console.log(error);
+                	  return;
+                	 }
+        	    });
+       		 });
+	    });
+	}
     });
+    res.render('success.html');
 });
 module.exports = router;
