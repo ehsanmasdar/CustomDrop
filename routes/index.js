53a4f78eba888957061fc17fb725f770ae9ed38c
Ehsan <ehsanmasdar@gmail.com>
2015-06-03 12:50:07 -0400

    Cookie caching, minor improvements

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,22 @@ var fs = require('fs');
 var busboy = require('connect-busboy');
 var inspect = require('util').inspect;
 var router = express.Router();
-
+var path = require('path')
 var Dropbox = require("dropbox");
+var cookieParser = require('cookie-parser');
 var client = new Dropbox.Client({
-    key: "4lq986lr70bcmjt",
+    key: "",
     secret: "",
     sandbox: false,
     token: ""
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
+ console.log("Cookies: ", req.cookies); 
+ if (req.cookies.lastname)
+ 	res.render('index.html',req.cookies);
+ else
+   res.render('index.html',{lastname:"",firstname:"",secret:""});
 });
 router.get('/testfile', function(req, res, next) {
   client.writeFile("test.txt", "sometext", function (error, stat) {
@@ -33,13 +38,13 @@ router.post('/upload', function(req, res) {
         if (fieldname == "firstname"){firstname = val;}
 	if (fieldname == "period"){period = val;}
 	if (fieldname == "secret"){secret = val;}
-	if (fieldname == "assignment"){assignment = val;}
-	if (fieldname == "extension"){
+	if (fieldname == "assignment"){
 		req.busboy.on('file', function (fieldname, file, filename) {
        		 console.log("Uploading: " + filename);
        		 file.on('data', function(data) {
-		 var finalfilename = period + "/" + lastname + firstname + secret + "_" + assignment + "." + val;
-           	 client.writeFile(finalfilename, data, function (error, stat) {
+		 var finalfilename = period + "/" + lastname + firstname + secret + "_" + val + path.extname(filename);
+           	 console.log("Final file name:",finalfilename);
+		 client.writeFile(finalfilename, data, function (error, stat) {
                		 if (error) {
                 	  console.log(error);
                 	  return;
@@ -47,8 +52,11 @@ router.post('/upload', function(req, res) {
         	    });
        		 });
 	    });
+	     res.cookie('lastname', lastname, { maxAge: 365*24*60*60*1000 });
+             res.cookie('firstname', firstname, { maxAge: 365*24*60*60*1000 });
+             res.cookie('secret', secret, { maxAge: 365*24*60*60*1000 });
+	     res.render('success.html');    	
 	}
     });
-    res.render('success.html');
 });
 module.exports = router;
